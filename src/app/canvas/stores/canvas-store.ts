88307359a5e89b4657586b922c305e7f03447779
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2025-02-22 19:36:38 +0100

    feat: init zustand stores

---
+++ src/app/canvas/stores/canvas-store.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+import type { Canvas, CanvasOptions } from 'fabric';
+import { produce } from 'immer';
+
+export const INIT_CANVAS_OPTIONS = {
+  width: 500,
+  height: 500,
+  backgroundColor: '#ffffff',
+} satisfies Partial<CanvasOptions>;
+
+interface CanvasState {
+  canvas: Canvas | null;
+  setCanvas: (canvas: Canvas | null) => void;
+  options: Partial<CanvasOptions>;
+  setOptions: (options: Partial<CanvasOptions>) => void;
+}
+
+export const useCanvasStore = create<CanvasState>()(
+  devtools(
+    (set) => ({
+      canvas: null,
+      setCanvas: (canvas) => set({ canvas }),
+      options: INIT_CANVAS_OPTIONS,
+      setOptions: (options) =>
+        set(
+          produce((draft) => {
+            draft.options = { ...draft.options, ...options };
+          })
+        ),
+    }),
+    { name: 'CanvasStore' }
+  )
+);
---
+++ src/app/canvas/stores/elements-store.ts
@@ -0,0 +1,43 @@
+import { create } from 'zustand';
+import { produce } from 'immer';
+import { FabricObject } from 'fabric';
+import { devtools } from 'zustand/middleware';
+
+export type ElementType = 'Rect' | 'Circle' | 'Textbox';
+
+export interface ElementObject extends FabricObject {}
+
+export interface ElementsState {
+  elements: ElementObject[];
+  addElement: (element: ElementObject) => void;
+  updateElement: (
+    id: ElementObject['id'],
+    updates: Partial<Omit<ElementObject, 'id'>>
+  ) => void;
+  removeElement: (id: ElementObject['id']) => void;
+}
+
+export const useElementsStore = create<ElementsState>()(
+  devtools(
+    (set) => ({
+      elements: [],
+
+      addElement: (element) =>
+        set((state) => ({ elements: [...state.elements, element] })),
+
+      updateElement: (id, updates) =>
+        set(
+          produce((state: ElementsState) => {
+            const el = state.elements.find((e) => e.id === id);
+            if (el) Object.assign(el, updates);
+          })
+        ),
+
+      removeElement: (id) =>
+        set((state) => ({
+          elements: state.elements.filter((el) => el.id !== id),
+        })),
+    }),
+    { name: 'ElementsStore' }
+  )
+);
