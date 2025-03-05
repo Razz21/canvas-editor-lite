c205a8d1c18f899a50cf4bae87a4bf3f49a5cfa1
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2025-03-05 15:00:21 +0100

    feat: custom themes

---
+++ src/lib/themes.ts
@@ -0,0 +1,43 @@
+export interface ColorTheme {
+  id: string;
+  name: string;
+}
+
+export const themes: ColorTheme[] = [
+  {
+    id: "light",
+    name: "Light",
+  },
+  {
+    id: "dark",
+    name: "Dark",
+  },
+  {
+    id: "purple",
+    name: "Purple",
+  },
+  {
+    id: "dark-purple",
+    name: "Dark Purple",
+  },
+  {
+    id: "midnight-galaxy",
+    name: "Midnight Galaxy",
+  },
+  {
+    id: "forest-breeze",
+    name: "Forest Breeze",
+  },
+  {
+    id: "desert-sunset",
+    name: "Desert Sunset",
+  },
+  {
+    id: "ocean-depths",
+    name: "Ocean Depths",
+  },
+  {
+    id: "aurora-nights",
+    name: "Aurora Nights",
+  },
+];
